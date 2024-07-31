9782ac5ea145d5795160d500d1e9fcc966835804
geotec-execucao <51171926+geotec-execucao@users.noreply.github.com>
2024-07-31 17:04:10 -0300

    configurações e primeiro cod em php
    Co-Authored-By: Gabriel Alves Oliveira <111614350+oliveiraprg@users.noreply.github.com>

---
+++ ex000/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu primeiro cod em PHP</title>
+</head>
+<body>
+    <h1>
+        <?php 
+         echo "Ola mundo";
+        ?>
+    </h1>
+    <p>Meu primeiro cod em PHP</p>
+</body>
+</html>
